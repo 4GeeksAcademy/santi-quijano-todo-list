1061dde6db427512cc04d8f8950298161e4113c7
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-11-14 00:03:07 +0000

    Finished Project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 function Home() {
+	// State Hook
+	const [tasks, setTasks] = useState([]);
+
+
+
+	const deleteTasks = tasks.filter((erase) => { index });
 
 	return (
 		<div className="container">
-			<h1>todos</h1>
+			<h1 className="text-muted">todos</h1>
+			<input type="text" placeholder="What needs to be done?" onKeyDown={(e) => { if (e.code === "Enter") setTasks([e.target.value, ...tasks]) }} />
 			<ul>
-				<li><input placeholder="What needs to be done" style={{ border: "none", width: "100%" }}></input></li>
-				<li>Something</li>
-				<li>Something</li>
-				<li>Something</li>
-				<li>Something</li>
+				{tasks.map((task, index) =>
+					<li>
+						<p>{task}</p>
+						<button onClick={() => deleteTask(index)}> X </button>
+					</li>
+				)}
+
 			</ul>
-			<div>Items left</div>
 		</div>
 	);
 };
+
 
 export default Home;
