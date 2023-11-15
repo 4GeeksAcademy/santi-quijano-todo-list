971bd141f583d6f6ada4602d8887b8d656b6178e
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-11-15 21:42:45 +0000

    Almost Finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,9 +4,9 @@ function Home() {
 	// State Hook
 	const [tasks, setTasks] = useState([]);
 
-
-
-	const deleteTasks = tasks.filter((erase) => { index });
+	const deleteTask = (indexToDelete) => {
+		setTasks((prevTasks) => prevTasks.filter((task, index) => index !== indexToDelete));
+	};
 
 	return (
 		<div className="container">
@@ -14,12 +14,12 @@ function Home() {
 			<input type="text" placeholder="What needs to be done?" onKeyDown={(e) => { if (e.code === "Enter") setTasks([e.target.value, ...tasks]) }} />
 			<ul>
 				{tasks.map((task, index) =>
-					<li>
+					<li key={index}>
 						<p>{task}</p>
 						<button onClick={() => deleteTask(index)}> X </button>
 					</li>
 				)}
-
+				<li className="list-group-item fontSizeSmall">{tasks.length === 1 ? "1 item" : `${tasks.length} items`}</li>
 			</ul>
 		</div>
 	);
