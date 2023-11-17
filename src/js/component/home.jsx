84dbe45130bd1b2a71debe63a882fca2f4d1dc99
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-11-17 21:21:35 +0000

    Almost Finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,29 +1,31 @@
 import React, { useState } from "react";
 
 function Home() {
-	// State Hook
+
 	const [tasks, setTasks] = useState([]);
 
 	const deleteTask = (indexToDelete) => {
 		setTasks((prevTasks) => prevTasks.filter((task, index) => index !== indexToDelete));
 	};
 
+
 	return (
 		<div className="container">
 			<h1 className="text-muted">todos</h1>
-			<input type="text" placeholder="What needs to be done?" onKeyDown={(e) => { if (e.code === "Enter") setTasks([e.target.value, ...tasks]) }} />
+			<input type="text" placeholder="What needs to be done?" onKeyDown={(e) => { if (e.code === "Enter") setTasks([e.target.value, ...tasks]); }} />
 			<ul>
-				{tasks.map((task, index) =>
+				{tasks.length === 0 && <li>No tasks, add a task</li>}
+				{tasks.map((task, index) => (
+
 					<li key={index}>
 						<p>{task}</p>
-						<button onClick={() => deleteTask(index)}> X </button>
+						<button className="erase" onClick={() => deleteTask(index)}>X</button>
 					</li>
-				)}
+				))}
 				<li className="list-group-item fontSizeSmall">{tasks.length === 1 ? "1 item" : `${tasks.length} items`}</li>
 			</ul>
+
 		</div>
 	);
-};
-
-
-export default Home;
+}
+export default Home; 
